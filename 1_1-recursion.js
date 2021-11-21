930366a6bc14a93c6e74e9e60a9fb9a5c93c5104
Liran-tal <taliran44@gmail.com>
2021-11-21 14:39:17 +0200

    1_5 - recursion.js

--- 1_1-recursion.js
+++ 1_1-recursion.js
@@ -31,10 +31,22 @@ function countDownInfiniteRec(num) {
 
 // .2.1
 
+function isStringValid(string) {
+	let valid = true;
+	let i = 0;
+	while ((i < string.length) && valid === true){
+		if ((string.charCodeAt(i) >= 128)) {
+			valid = false;
+		}
+	}
+
+	return valid;
+}
+
 function reverseStringLibFuncs(string) {
-	// if (!isStringValid(string)) {
-	// 	return -1;
-	// }
+	if (!isStringValid(string)) {
+		return -1;
+	}
 
 	return string.split('').reverse().join('');
 }
@@ -46,9 +58,9 @@ function reverseStringLibFuncs(string) {
 
 function reverseStringLoop(string) {
 	const res = [];
-	// if (!isStringValid(string)) {
-	// 	return -1;
-	// }
+	if (!isStringValid(string)) {
+		return -1;
+	}
 
 	for (let i = string.length - 1; i >= 0; -- i){
 		res.push(string.charAt(i));
@@ -65,9 +77,11 @@ function reverseStringLoop(string) {
 function reverseStringRecursive(string) {
 	let res = [];
 	let counter = 0;
-	// if (!isStringValid(string)) {
-	// 	return -1;
-	// }
+
+	if (!isStringValid(string)) {
+		return -1;
+	}
+
 	res.push(reverseStringRec(string, res, counter));
 	return res.join('')
 }
@@ -86,6 +100,14 @@ function reverseStringRec(string, res, counter) {
 
 // .3
 
+function fibonacci(num){
+	if (num < 0 || num > 30) {
+		return -1;
+	}
+
+	return F(num);
+}
+
 function F(n) {
 	let res = n
 	if (n > 1) {
@@ -95,5 +117,45 @@ function F(n) {
 	return res
 }
 
-console.log('F(4): ', F(6));
+// console.log('fibonacci(6): ', fibonacci(6));
 
+
+// .4
+
+// if (base == 0) return 0;
+// if (base == 0 && num === 1) return 0;
+// if (num == 0) return -1;
+// function findExp(base, num) {
+// 	let exp = 0;
+// 	return num / base !== 1 ? ++ exp : findExp(base, num / base)
+// }
+
+// console.log(findExp(4, 16));
+// console.log(findExp(4, 64));
+// console.log(findExp(5, 125));
+
+
+//.5
+
+const array = [1,2,4,6,8,10,56];
+
+function search(array, num) {
+	
+	return binSearchRec(array, num, 0, array.length - 1);
+}
+
+function binSearchRec(array, num, start, end){
+	let mid = Math.floor(start + (end - start) / 2);
+	if (num === array[mid]) {
+		return mid;
+	} else if (num > array[mid]) {
+		start = mid + 1;
+	} else {
+		end = mid - 1;
+	}
+
+	return binSearchRec(array, num, start, end);
+}
+
+console.log("search(10): ", search(array, 10))
+console.log("search(10): ", search(array, 2))
